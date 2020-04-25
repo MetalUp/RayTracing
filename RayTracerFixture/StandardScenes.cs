839ebcfdcfce3e9b654ca9300cc72034ef2d128f
Richard Pawson <rpawson@nakedobjects.net>
2020-04-25 14:51:34 +0100

    Missed check-in

--- RayTracerFixture/StandardScenes.cs
+++ RayTracerFixture/StandardScenes.cs
@@ -4,9 +4,9 @@
     {
         public static readonly Scene DefaultScene = new Scene(DefaultThings(), DefaultLightSources(), DefaultCamera());
 
-        private static IThing[] DefaultThings()
+        private static Thing[] DefaultThings()
         {
-            return new IThing[] {
+            return new Thing[] {
                                 new Plane(new Vector3(0,1,0),0, StandardSurfaces.CheckerBoard),
                                 new Sphere(new Vector3(0,0.5,0),0.5,StandardSurfaces.Shiny),
                                 new Sphere(new Vector3(1,1,1),0.2,StandardSurfaces.Matt),
@@ -17,10 +17,10 @@
         private static LightSource[] DefaultLightSources()
         {
             return new LightSource[] {
-                                new LightSource(new Vector3(-2,2.5,0), new Color(.49,.07,.07)),
-                                new LightSource(new Vector3(1.5,2.5,1.5),new Color(.07,.07,.49)),
-                                new LightSource(new Vector3(1.5,2.5,-1.5),new Color(.07,.49,.071)),
-                                new LightSource(new Vector3(0,3.5,0),new Color(.21,.21,.35))
+                                new LightSource(new Vector3(-2,2.5,0), new Colour(.49,.07,.07)),
+                                new LightSource(new Vector3(1.5,2.5,1.5),new Colour(.07,.07,.49)),
+                                new LightSource(new Vector3(1.5,2.5,-1.5),new Colour(.07,.49,.071)),
+                                new LightSource(new Vector3(0,3.5,0),new Colour(.21,.21,.35))
                     };
         }
 
--- RayTracerModel/Sphere.cs
+++ RayTracerModel/Sphere.cs
@@ -4,18 +4,16 @@ namespace RayTracer
 {
     public class Sphere : Thing
     {
-        public SurfaceTexture Surface { get; private set; }
         public Vector3 Centre { get; private set; }
         public double Radius { get; private set; }
 
-        public Sphere(Vector3 centre, double radius, SurfaceTexture surface) 
+        public Sphere(Vector3 centre, double radius, SurfaceTexture surface) : base(surface)
         {
             Centre = centre;
             Radius = radius;
-            Surface = surface;
         }
 
-        public Intersection CalculateIntersection(Ray withRay)
+        public override Intersection CalculateIntersection(Ray withRay)
         {
             Vector3 eo = Centre - withRay.Start;
             double v = eo.DotProduct(withRay.Dir);
@@ -33,7 +31,7 @@ namespace RayTracer
             return new Intersection(this, withRay, dist);
         }
 
-        public Vector3 CalculateNormal(Vector3 surfacePosition)
+        public override Vector3 CalculateNormal(Vector3 surfacePosition)
         {
             return (surfacePosition - Centre).Normalized();
         }
